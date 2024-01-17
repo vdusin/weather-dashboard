a916a33f45b77a1f0952f4c7b0bee433393269ce
vdusin <vanessadusin@gmail.com>
2024-01-16 21:53:52 -0600

    5day weather moved header

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ var apiKey = "aa4945bfa1063d57a5eed0fc5d29e13a"
 var searchBar= document.querySelector(".searchBar")
 var Button= document.querySelector(".button")
 var cityName= document.querySelector(".cityName")
+// var currentDate=document.querySelector(".date")
 var currentTemp=document.querySelector(".temp")
 var currentWind=document.querySelector(".wind")
 var currentHumidity=document.querySelector(".humidity")
@@ -11,26 +12,34 @@ var fivedayContainer=document.querySelector(".fiveDay")
 function renderWeather(data){
     console.log(data)
     cityName.textContent= data.city.name
-    // var currentDate = dayjs().format('MMM D, YYYY');
+    // currentDate.textContent = data.dt_txt
     currentTemp.textContent= "Temperature: "+ data.list[0].main.temp
     currentWind.textContent= "Wind: "+ data.list[0].wind.speed+ " mph"
     currentHumidity.textContent= "Humidity: "+ data.list[0].main.humidity+ "%"
 }
 
 function render5day(data){
-    for (var i = 0; i < data.list.length; i=i+8) {
-      console.log(data.list[i])
-      
-      // five day header (repeats.why)
-      var fivedayHeader = document.createElement("h3");
+
+  fivedayContainer.innerHTML=""
+
+  var fivedayHeader = document.createElement("h3");
       fivedayHeader.textContent = "Five-Day Forecast: " + searchBar.value;
       fivedayContainer.append(fivedayHeader)
 
-      // weather info for 5 day forecast (breaks.why)
-      // var currentDate = dayjs().format('MMM D, YYYY');
-      // var temp= "Temperature: "+ data.list[0].main.temp
-      // var wind= "Wind: "+ data.list[0].wind.speed+ " mph"
-      // var humidity= "Humidity: "+ data.list[0].main.humidity+ "%"
+    for (var i = 0; i < data.list.length; i=i+8) {
+      console.log(data.list[i])
+    
+      var tempEl = document.createElement("p")
+      tempEl.innerHTML = "Temperature: "+ data.list[i].main.temp
+      fivedayContainer.append(tempEl)
+
+      var windEl = document.createElement("p")
+      windEl.innerHTML = "Wind: "+ data.list[0].wind.speed+ " mph"
+      fivedayContainer.append(windEl)
+
+      var humidityEL = document.createElement("p")
+      humidityEL.innerHTML = "Humidity: "+ data.list[0].main.humidity+ "%"
+      fivedayContainer.append(humidityEL)
       }
 }
 
